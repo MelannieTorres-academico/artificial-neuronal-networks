bdc8597cbefc302e06e4a38e7eb0ef5cf95f9fca
Bernardo Gómez Romero <a01209704@itesm.mx>
2018-11-09 14:06:45 -0600

    finishing prossesing input

--- ann.py
+++ ann.py
@@ -1,5 +1,6 @@
 import fileinput
 
+
 def main():
     file_input = fileinput.input()
     training_set = []
@@ -9,15 +10,19 @@ def main():
     m_size = int(file_input[1])
     n_size = int(file_input[2])
 
-
     for i in range(m_size):
-        training_set.append(file_input[i+3])
-        #separate in dimensions d
+        training_set.append(file_input[i+3].replace(" ", "").replace("\t", "").replace("\n", "").split(','))
+        # separate in dimensions d
 
     for i in range(n_size):
-        test_set.append(file_input[i+3+m_size])
-        #separate in dimensions d
+        test_set.append(file_input[i+3+m_size].replace(" ", "").replace("\t", "").replace("\n", "").split(','))
+        # separate in dimensions d
 
+    print(d)
+    print(m_size)
+    print(n_size)
+    print(training_set)
+    print(test_set)
 
 
 if __name__ == "__main__":
