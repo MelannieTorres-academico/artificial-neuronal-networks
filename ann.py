2e5b7a057b0c4fd018defa61b7d2cbee68c19a62
MelannieTorres <melannie@mienvio.mx>
2018-11-09 21:50:55 -0600

    first approach

--- ann.py
+++ ann.py
@@ -3,17 +3,50 @@ from random import random
 
 def initialize_weights(num_weights):
     weights = []
-    for i in range(1, num_weights):
-        weights.push(random() * 0.01)
+    for i in range(0, num_weights):
+        weights.append(random() * 0.01)
     return weights
 
 
-def ann(d, l_rate, x_train_set, y_train_set):
+def ann_training(d, l_rate, x_train_set, y_train_set, x_test_set):
     weights = initialize_weights(d + 1)
     error = 1
     num_iterations = 0
-    while (error > 0 and num_iterations < 1000):
-        for x in x_train_set:
+    while (error > 0 and num_iterations < 100000):
+        acum = 0
+        error = 0
+        num_iterations = num_iterations+1
+        for key in range(0, len(x_train_set)):
+            for i in range(0, len(x_train_set[key])):
+                acum += x_train_set[key][i]*weights[i]
+
+            if(acum >= 0):
+                y_hat = 1
+            else:
+                y_hat = 0
+
+            y_difference = y_train_set[key]-y_hat
+            if(y_difference!= 0):
+                error = error+1
+            delta_w = (y_train_set[key]-y_hat)*l_rate*x_train_set[key][i]
+            weights[i] += delta_w
+
+
+    if(error!=0):
+        print("no solution found")
+    else:
+        for key in range(0, len(x_test_set)):
+            acum = 0
+            for i in range(0, len(x_test_set[key])):
+                acum += x_test_set[key][i]*weights[i]
+            if(acum >= 0):
+                y_hat = 1
+            else:
+                y_hat = 0
+            print( y_hat)
+
+
+
 
 
 
@@ -22,7 +55,8 @@ def main():
     file_input = fileinput.input()
     x_training_set = []
     y_training_set = []
-    test_set = []
+    x_test_set = []
+    l_rate = 0.01
 
     d = int(file_input[0])
     m_size = int(file_input[1])
@@ -31,15 +65,17 @@ def main():
     for i in range(m_size):
         training_set_str = file_input[i + 3].replace(" ", "").replace("\t", "").replace("\n", "").split(',')
         training_set_float = [float(x) for x in training_set_str]
-        x_training_row = training_set_float[0:d-1].append(1)
+        x_training_row = training_set_float[0:d]
+        x_training_row.append(1.0)
         x_training_set.append(x_training_row)
         y_training_set.append(training_set_float[d])
-        # separate in dimensions d
 
     for i in range(n_size):
-        test_set_str = file_input[i + 3].replace(" ", "").replace("\t", "").replace("\n", "").split(',')
-        test_set.append([float(x) for x in test_set_str])
-        # separate in dimensions d
+        test_set_str = file_input[i + m_size +3].replace(" ", "").replace("\t", "").replace("\n", "").split(',')
+        x_test_set.append([float(x) for x in test_set_str])
+        x_test_set.append(1.0)
+
+    ann_training(d, l_rate, x_training_set, y_training_set, x_test_set)
 
 
 if __name__ == "__main__":
