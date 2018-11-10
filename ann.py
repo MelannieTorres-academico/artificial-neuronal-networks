f8e3066b0d0281a1b94ce4a0ed1b65d3e137dcaa
MelannieTorres <melannie@mienvio.mx>
2018-11-09 22:19:17 -0600

    second approach

--- ann.py
+++ ann.py
@@ -4,7 +4,7 @@ from random import random
 def initialize_weights(num_weights):
     weights = []
     for i in range(0, num_weights):
-        weights.append(random() * 0.01)
+        weights.append(random())
     return weights
 
 
@@ -29,8 +29,7 @@ def ann_training(d, l_rate, x_train_set, y_train_set, x_test_set):
             if(y_difference!= 0):
                 error = error+1
             delta_w = (y_train_set[key]-y_hat)*l_rate*x_train_set[key][i]
-            weights[i] += delta_w
-
+            weights[i] = weights[i] + delta_w
 
     if(error!=0):
         print("no solution found")
@@ -67,15 +66,19 @@ def main():
         training_set_float = [float(x) for x in training_set_str]
         x_training_row = training_set_float[0:d]
         x_training_row.append(1.0)
+
         x_training_set.append(x_training_row)
         y_training_set.append(training_set_float[d])
 
     for i in range(n_size):
         test_set_str = file_input[i + m_size +3].replace(" ", "").replace("\t", "").replace("\n", "").split(',')
-        x_test_set.append([float(x) for x in test_set_str])
-        x_test_set.append(1.0)
+        test_set_float = [float(x) for x in test_set_str]
+        x_test_row = test_set_float[0:d]
+        x_test_row.append(1.0)
+        x_test_set.append(x_test_row)
 
     ann_training(d, l_rate, x_training_set, y_training_set, x_test_set)
+
 
 
 if __name__ == "__main__":
