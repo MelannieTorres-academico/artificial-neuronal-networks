3905fcaa91b5657c7f25be5c45a2df1a51fe5cf4
Bernardo Gómez Romero <a01209704@itesm.mx>
2018-11-09 19:01:53 -0600

    wip

--- ann.py
+++ ann.py
@@ -1,9 +1,27 @@
 import fileinput
+from random import random
+
+def initialize_weights(num_weights):
+    weights = []
+    for i in range(1, num_weights):
+        weights.push(random() * 0.01)
+    return weights
+
+
+def ann(d, l_rate, x_train_set, y_train_set):
+    weights = initialize_weights(d + 1)
+    error = 1
+    num_iterations = 0
+    while (error > 0 and num_iterations < 1000):
+        for x in x_train_set:
+
+
 
 
 def main():
     file_input = fileinput.input()
-    training_set = []
+    x_training_set = []
+    y_training_set = []
     test_set = []
 
     d = int(file_input[0])
@@ -11,18 +29,17 @@ def main():
     n_size = int(file_input[2])
 
     for i in range(m_size):
-        training_set.append(file_input[i+3].replace(" ", "").replace("\t", "").replace("\n", "").split(','))
+        training_set_str = file_input[i + 3].replace(" ", "").replace("\t", "").replace("\n", "").split(',')
+        training_set_float = [float(x) for x in training_set_str]
+        x_training_row = training_set_float[0:d-1].append(1)
+        x_training_set.append(x_training_row)
+        y_training_set.append(training_set_float[d])
         # separate in dimensions d
 
     for i in range(n_size):
-        test_set.append(file_input[i+3+m_size].replace(" ", "").replace("\t", "").replace("\n", "").split(','))
+        test_set_str = file_input[i + 3].replace(" ", "").replace("\t", "").replace("\n", "").split(',')
+        test_set.append([float(x) for x in test_set_str])
         # separate in dimensions d
-
-    print(d)
-    print(m_size)
-    print(n_size)
-    print(training_set)
-    print(test_set)
 
 
 if __name__ == "__main__":
